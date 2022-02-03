f8c8da8b2cf19e4ab2dded98ed49aa13b07f3890
jbride <jbride2001@yahoo.com>
2022-02-02 22:42:39 -0700

    kafka:  first draft

--- src/main/java/com/redhat/himss/Routes.java
+++ src/main/java/com/redhat/himss/Routes.java
@@ -2,27 +2,21 @@ package com.redhat.himss;
 
 import org.jboss.logging.Logger;
 
-import io.smallrye.mutiny.operators.multi.builders.CollectionBasedMulti;
-
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
 
 import javax.activation.DataHandler;
 
-import org.apache.camel.Body;
 import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
-import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.attachment.AttachmentMessage;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.tarfile.TarSplitter;
 import org.apache.camel.model.rest.RestBindingMode;
-import org.apache.camel.model.dataformat.BindyType;
 
 /**
  * Camel route definitions.
@@ -30,6 +24,7 @@ import org.apache.camel.model.dataformat.BindyType;
 public class Routes extends RouteBuilder {
 
     private static final String RESPONSE_HEADER="RESPONSE_HEADER";
+    private static final String FILE_NAME_HEADER="FILE_NAME_HEADER";
     private static Logger log = Logger.getLogger(Routes.class);
 
 
@@ -63,7 +58,7 @@ public class Routes extends RouteBuilder {
           .routeId("verifyPayload")
           .setHeader(RESPONSE_HEADER, constant("ALL FILES PERSISTED"))
           .doTry()
-                .process(new PayloadValidator())
+                .process(new GzipPayloadValidator())
                 .process(e -> {
                     Map<String, DataHandler> attachments = e.getIn(AttachmentMessage.class).getAttachments();
                     Collection<byte[]> aList = new ArrayList<byte[]>();
@@ -85,7 +80,6 @@ public class Routes extends RouteBuilder {
             
         from("seda:writeVerifiedPayloadToFilesystem")
             .routeId("direct:writeVerifiedPayloadToFilesystem")
-            .log("writeVerifiedPayloadToFilesystem")
             .split(body())
             .to("file:{{himss.scm.gzip.location}}");
 
@@ -98,10 +92,18 @@ public class Routes extends RouteBuilder {
             .routeId("direct:unpackGzip")
                 .unmarshal()
                 .gzipDeflater()
-                .log("file = ${header.CamelFileName}}")
+                //.log("file = ${header.CamelFileName}}")
                 .split(new TarSplitter())
                   .streaming()
-                  .to("direct:processTextFile")
+                  .process(e -> {
+                      String originalFileName = (String)e.getIn().getHeader("CamelFileName");
+                      int lastDirIndex = originalFileName.lastIndexOf("/");
+                      String parsedFileName = originalFileName.substring(lastDirIndex+1);
+                      log.debug("prepKafkaProducer()  originalFileName = "+originalFileName+" : parsedFileName = "+parsedFileName);
+                      e.getIn().setHeader(FILE_NAME_HEADER, parsedFileName);
+                  })
+                  //.to("direct:processTextFile")
+                  .to("kafka:{{scm_topic_name}}?brokers={{kafka.bootstrap.servers}}")
                 .end();
 
         from("direct:processTextFile")
@@ -109,20 +111,71 @@ public class Routes extends RouteBuilder {
             .log("file = ${header.CamelFileName}}")
             .end();
 
+        
+        /************               Consume from Kafka          *****************/
+        from("kafka:{{scm_topic_name}}?brokers={{kafka.bootstrap.servers}}&groupId=scm&autoOffsetReset=earliest")
+            .doTry()
+                .process(new CSVPayloadValidator())
+                .process(new CSVPayloadProcessor())
+            .doCatch(ValidationException.class)
+                .log(LoggingLevel.ERROR, exceptionMessage().toString())
+            .end();
+
 
     }
 
-    class PayloadValidator implements Processor {
+    class GzipPayloadValidator implements Processor {
 
         @Override
         public void process(Exchange exchange) throws ValidationException {
+
             AttachmentMessage attMsg = exchange.getIn(AttachmentMessage.class);
             Set<String> attachmentNames = attMsg.getAttachmentNames();
             for(String aName : attachmentNames) {
-                log.info("PayloadValidator.process()   fileName = "+aName);
+                log.info("GzipPayloadValidator.process()   fileName = "+aName);
                 if(!aName.endsWith("tgz") && !aName.endsWith("tar.gz"))
                   throw new ValidationException("Invalid file suffix: "+aName);
             }
+        }
+    }
+
+
+
+    class CSVPayloadValidator implements Processor {
+
+        @Override
+        public void process(Exchange exchange) throws ValidationException {
+
+            Object fileNameHeaderObj = exchange.getIn().getHeader(FILE_NAME_HEADER);
+            if(fileNameHeaderObj == null)
+              throw new ValidationException("Must pass kafka header of: "+FILE_NAME_HEADER);
+
+            Object bObj = exchange.getIn().getBody();
+            if(!bObj.getClass().getName().equals(String.class.getName()))
+              throw new ValidationException("Payload not of type String : "+bObj.getClass().getName());
+
+        }
+    }
+
+    class CSVPayloadProcessor implements Processor {
+
+        @Override
+        public void process(Exchange exchange){
+            try{
+                byte[] fileNameHeaderBytes = (byte[])exchange.getIn().getHeader(FILE_NAME_HEADER);
+                String fileNameHeader = new String(fileNameHeaderBytes);
+                String body = (String)exchange.getIn().getBody();
+                String[] rows = body.split("\n");
+                log.info("CSVPayloadProcessor.process() "+fileNameHeader+" :   # of rows = "+rows.length);
+                for(String row : rows){
+                    //log.info("row = "+row);
+                    String[] fields = row.split("\\|");
+                    //log.info("# of fields = "+fields.length);
+                }
+            }catch(Throwable x){
+                x.printStackTrace();
+            }
+
         }
     }
 
