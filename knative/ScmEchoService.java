56e8b46950e8c85dbd65ce5fee6cfb2cda77c7b5
gbengataylor <gbenga@gbenga.com>
2022-02-03 16:10:43 -0500

    fixed autoscaling

--- knative/ScmEchoService.java
+++ knative/ScmEchoService.java
@@ -1,5 +1,5 @@
 // camel-k: language=java
-//kamel run ScmEchoService.java --dev --name scm-echo -d camel-rest --trait knative-service.enabled=true
+//kamel run ScmEchoService.java --dev --name scm-echo -d camel-rest --trait knative-service.enabled=true --trait knative-service.autoscaling-target=1 --trait knative-service.max-scale=10
 
 
 import org.apache.camel.builder.RouteBuilder;
