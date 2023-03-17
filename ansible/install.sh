96c647204e4d66917da8d2b136e2fd1ae91f24e8
Jon Keam <jpkeam@gmail.com>
2023-03-16 21:37:01 -0400

    update to latest operators, remove scale downs and ups in favor of fips patch. also fixed comparison for fips

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --extra-vars '{"fips_enabled_cluster":"true"}' playbooks/install.yml
+ansible-playbook --extra-vars '{"fips_enabled_cluster": true}' playbooks/install.yml
